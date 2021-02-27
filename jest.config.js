fe943c718e63120c496788518ec9d7fe25e1ce6d
Yan Soares <yts.1995@gmail.com>
2021-02-27 16:03:53 -0300

    chore: update jest configs

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   roots: ['<rootDir>/src/tests'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/tests/**/index.ts',
     '!<rootDir>/src/main/**'
   ],
   coverageDirectory: 'coverage',
