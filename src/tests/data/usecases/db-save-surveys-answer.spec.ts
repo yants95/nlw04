89ff982db47693cde43a0fc21c74fe0a295c7996
Yan Soares <yts.1995@gmail.com>
2021-03-02 12:19:15 -0300

    test: ensure DbSaveSurveyAnswer calls SaveSurveyAnswer with correct values

---
+++ src/tests/data/usecases/db-save-surveys-answer.spec.ts
@@ -0,0 +1,29 @@
+import { DbSaveSurveyAnswer } from '@/data/usecases'
+import { SaveSurveysAnswerRepositorySpy } from '@/tests/data/mocks'
+import { mockAddSurveyAnswerParams } from '@/tests/domain/mocks'
+
+type SutTypes = {
+  sut: DbSaveSurveyAnswer
+  saveSurveysAnwserRepositorySpy: SaveSurveysAnswerRepositorySpy
+}
+
+const makeSut = (): SutTypes => {
+  const saveSurveysAnwserRepositorySpy = new SaveSurveysAnswerRepositorySpy()
+  const sut = new DbSaveSurveyAnswer(saveSurveysAnwserRepositorySpy)
+  return {
+    sut,
+    saveSurveysAnwserRepositorySpy
+  }
+}
+
+describe('DbSaveSurveyAnswer Usecase', () => {
+  test('should be able to create a new answer to an specific survey', async () => {
+    const { sut, saveSurveysAnwserRepositorySpy } = makeSut()
+    const addASurveyAnswerParams = mockAddSurveyAnswerParams()
+    await sut.save(mockAddSurveyAnswerParams())
+    expect(saveSurveysAnwserRepositorySpy.params).toEqual({
+      value: addASurveyAnswerParams.value,
+      user_id: addASurveyAnswerParams.user_id
+    })
+  })
+})
